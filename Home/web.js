49edb33308c1491917dc4cd625fbf53bd8231906
Grace <graceadegunle16@gmail.com>
2024-03-20 13:57:33 +0100

    commit

--- Home/web.js
+++ Home/web.js
@@ -1,39 +1,5 @@
-const searchMeal = () => {
-  const searchInput = document.getElementById("searchInput").value;
+const navigateButton = document.getElementById("navigateButton");
 
-  if (searchInput.trim() !== "") {
-    const apiUrl = `www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
-
-    fetch(apiUrl)
-      .then((resp) => {
-        if (!resp.ok) {
-          throw new Error(`Status not  found ${resp.statusText}`);
-        }
-        return resp.json();
-      })
-      .then((data) => {
-        displayResults(data.meals);
-      })
-      .catch((error) => {
-        console.Error("There was a problem with the fetch operation:", error);
-      });
-  } else {
-    console.log("Enter a meal name");
-  }
-};
-const displayResults = (meals) => {
-  const mealResults = document.getElementById("mealResults");
-  mealResults.innerHTML = "";
-
-  if (meals) {
-    meals.forEach((meal) => {
-      const mealDiv = document.createElement("div");
-      mealDiv.innerHTML = `<h3>${meal.strMeal}</h3>
-                                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                                 <p>${meal.strInstructions}</p>`;
-      mealResults.appendChild(mealDiv);
-    });
-  } else {
-    mealResults.innerHTML = "No results found.";
-  }
-};
+navigateButton.addEventListener("click", () => {
+  window.location.href = "../Recipes/Recipes.html";
+});
--- Recipes/Recipes.js
+++ Recipes/Recipes.js
@@ -1,5 +1,34 @@
-// Add this JavaScript to your existing script.js or create a new one
+const recipeCards = document.querySelectorAll(".recipe-card");
 
-function printRecipe() {
-  window.print();
-}
+recipeCard.addEventListener("mouseover", () => {
+  recipeCard.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.3)";
+  recipeCard.style.backgroundColor = "#166d3b";
+  recipeCard.style.transition = "transform 0.3s";
+  recipeCard.style.color = "#fff";
+  recipeCard.style.transform = "scale(1.1)";
+});
+
+recipeCard.addEventListener("mouseout", () => {
+  recipeCard.style.boxShadow = "none";
+  recipeCard.style.backgroundColor = "#fff";
+  recipeCard.style.color = "#166d3b";
+  recipeCard.style.transform = "scale(1)";
+});
+
+recipeCards.forEach((recipeCard) => {
+  // Add mouseover event listener
+  recipeCard.addEventListener("mouseover", function () {
+    // Add box shadow, change background color, and text color
+    this.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.3)";
+    this.style.backgroundColor = "#007bff";
+    this.style.color = "#fff";
+  });
+
+  // Add mouseout event listener
+  recipeCard.addEventListener("mouseout", function () {
+    // Remove box shadow, revert background color, and text color
+    this.style.boxShadow = "none";
+    this.style.backgroundColor = "#f0f0f0";
+    this.style.color = "initial";
+  });
+});
